dfa01d2178a799c943917fe3ecb7899816b58fb3
AvivLugasi <avivlugasi541@gmail.com>
2021-11-22 13:15:06 +0200

    sorted the classes

--- ManagerFly/src/control/ReportsLogic.java
+++ ManagerFly/src/control/ReportsLogic.java
@@ -41,7 +41,15 @@ public class ReportsLogic {
 			_instance = new ReportsLogic();
 		return _instance;
 	}
-	
+	/**
+	 * 
+	 * * The method create a report of the biggestFlights query which selects flights that occurred between 2 dates that the user gave, and contains at least
+	 * a given number of tourists seats.
+	 * @param seatsNum = the lower bound of tourists seats that the flight should contain. 
+	 * @param from	= the date which  the method start collect flights.
+	 * @param until = the date which  the method end collect flights.
+	 * @return a new frame that contain the report
+	 */
 	public  JFrame compileBiggestFlights(int seatNum, LocalDate from, LocalDate until) 
 	{
 		
@@ -78,162 +86,5 @@ public class ReportsLogic {
 
 	}
 	
-	/**
-	 * *****************************This is a test method***********************************<br>
-	 * The method select flights that occurred between 2 dates that the user gave, and contains at least
-	 * a given number of tourists seats.
-	 * @param seatsNum = the lower bound of tourists seats that the flight should contain. 
-	 * @param from	= the date which  the method start collect flights.
-	 * @param until = the date which  the method end collect flights.
-	 * @return a HashMap of flights as key ,and string List of city and country of the departure airport, 
-	 * and the landing airport.
-	 */
-	public static ArrayList<Flight> getBiggestFlights(int seatsNum, LocalDate from, LocalDate until) {
-
-		ArrayList<Flight> toReturn = new ArrayList<Flight>();
-		try {
-			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-			try (Connection conn = DriverManager.getConnection(Consts.CONN_STR);
-					Statement stmt = conn.createStatement()){
-				
-				int i = 1;
-				String date1 = "#" + from.getMonthValue() + "/" + from.getDayOfMonth() + "/" + from.getYear() + "#";
-				String date2 = "#" + until.getMonthValue() + "/" + until.getDayOfMonth() + "/" + until.getYear() + "#";				
-				String query = Consts.qryBiggestFlightsStr(date1, date2, seatsNum);
-				ResultSet rs = stmt.executeQuery(query);{	
-					while (rs.next()) 
-					{
-						i=1;
-						int flightID = rs.getInt(i++);
-						String deptCounty = rs.getString(i++);
-						String deptCity = rs.getString(i++);
-						String destCounty = rs.getString(i++);
-						String destCity = rs.getString(i++);
-						LocalDateTime depTime = rs.getTimestamp(i++).toInstant()
-							      .atZone(ZoneId.systemDefault())
-							      .toLocalDateTime();
-						LocalDateTime arrTime = rs.getTimestamp(i++).toInstant()
-							      .atZone(ZoneId.systemDefault())
-							      .toLocalDateTime();
-						String flightStatus = rs.getString(i++);
-						AirPort dep = new AirPort(0, deptCity,deptCounty);
-						AirPort dest = new AirPort(0, destCity,destCounty);
-						
-						Flight f = new Flight(flightID, depTime, arrTime, flightStatus,null, dep, dest);
-						toReturn.add(f);
-					}
-				
-				}
-				
-				
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return toReturn;
-	}
-	/**
-	 * return a HashMap of planes and the number of tourists seats in them
-	 * @return
-	 */
-	public static HashMap<AirPlane,Integer> getSeatCntByPlane() {
-		
-		HashMap<AirPlane,Integer> results = new HashMap<AirPlane,Integer>();
-		try {
-			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-			try (Connection conn = DriverManager.getConnection(Consts.CONN_STR);
-					PreparedStatement stmt = conn.prepareStatement(Consts.SQL_SEL_CNT_TSEAT_BY_PLANE);
-					ResultSet rs = stmt.executeQuery()) {
-				while (rs.next()) {
-					int i = 1;
-					AirPlane  aP = new AirPlane(rs.getString(i++));
-					int count = rs.getInt(i++);
-					results.put(aP, count);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return results;
-	}
-	
-	/**
-	 * return a list of flight which occurred in a range of dated given in the parameters
-	 * @param from = the date which  the method start collect flights.
-	 * @param until = the date which  the method end collect flights.
-	 * @return array list of flight sorted by the country and city of the destination and by dates
-	 */
-	public static ArrayList<Flight> getFlightsInRange(LocalDate from, LocalDate until){
-		
-		ArrayList<Flight> toReturn = new ArrayList<Flight>();
-		try {
-			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-			try (Connection conn = DriverManager.getConnection(Consts.CONN_STR);
-					Statement stmt = conn.createStatement()) {
-						int i = 1;	
-						String date1 = "#" + from.getMonthValue() + "/" + from.getDayOfMonth() + "/" + from.getYear() + "#";
-						String date2 = "#" + until.getMonthValue() + "/" + until.getDayOfMonth() + "/" + until.getYear() + "#";
-						String query = Consts.flightsInRangeQuery(date1, date2);
-						ResultSet rs = stmt.executeQuery(query);{	
-							while (rs.next()) 
-							{
-								i=1;
-								int flightID = rs.getInt(i++);
-								String tailNum = rs.getString(i++);
-								ArrayList<String> ar = new ArrayList<String>();
-								String deptCounty = rs.getString(i++);
-								String deptCity = rs.getString(i++);
-								String destCounty = rs.getString(i++);
-								String destCity = rs.getString(i++);
-								LocalDateTime depTime = rs.getTimestamp(i++).toInstant()
-									      .atZone(ZoneId.systemDefault())
-									      .toLocalDateTime();
-								LocalDateTime arrTime = rs.getTimestamp(i++).toInstant()
-									      .atZone(ZoneId.systemDefault())
-									      .toLocalDateTime();
-								String flightStatus = rs.getString(i++);
-								int deptID = rs.getInt(i++);
-								int destID = rs.getInt(i++);
-								ar.add(deptCounty);ar.add(deptCity);
-								ar.add(destCounty);ar.add(destCity);
-								Flight f = new Flight(flightID, depTime, arrTime, flightStatus,new AirPlane(tailNum), new AirPort(deptID,deptCity,deptCounty), new AirPort(destID,destCity,destCounty));
-								toReturn.add(f);
-							}
-						
-						}
-						
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return toReturn;
-	}
-	
-	///Testing method
-	public static void main(String args[]){
-		 
-		LocalDate from = LocalDate.of(2022, 2, 9);
-		LocalDate until = LocalDate.of(2022, 2, 14);
-		int attNum = 2;
-
-		HashMap<AirPlane,Integer> planeCntMap = getSeatCntByPlane();
-		for (Entry<AirPlane,Integer> entry : planeCntMap.entrySet()) {
-	         AirPlane f = entry.getKey();
-	         int count = entry.getValue();
-			 System.out.println("plane = " + f.getTailNum() + " seat count = " + count);
-		 }
-		 ArrayList<Flight> toReturn = getBiggestFlights(attNum, from, until);
-		 for(Flight f: toReturn) {
-			 System.out.println(f.toStringForReport());
-		 }
-		 
-	}
-		
 }
 	
--- ManagerFly/src/util/Consts.java
+++ ManagerFly/src/util/Consts.java
@@ -56,47 +56,17 @@ public class Consts {
 	public static final int MAX_TAIL_NUM = 15;
 	
 	
-	/*----------------------------------------- REPORTS QUERIES -----------------------------------------*/
-	/*-----------------------------------------for BiggestFlyReport--------------------------------------*/
-	/**
-	 * return a list of planes and the number of tourists seats in them  
-	 */
-	public static final String SQL_SEL_CNT_TSEAT_BY_PLANE = "SELECT AirPlaneTbl.TailNum, Count(FlightSeatTbl.ID) AS CountOfID\r\n"
-			+ "FROM AirPlaneTbl INNER JOIN FlightSeatTbl ON AirPlaneTbl.TailNum = FlightSeatTbl.TailNum\r\n"
-			+ "WHERE (((FlightSeatTbl.type) Like \"tourists\"))\r\n"
-			+ "GROUP BY AirPlaneTbl.TailNum;";
-	/**
-	 * String that present a query to return flights in range of to dates
-	 * @param date1 = string of the date which  the method start collect flights.
-	 * @param date2 = string of the date which  the method end collect flights.
-	 * @return a string that present the qryFlightsInRange
-	 */
-	public static String flightsInRangeQuery(String date1, String date2) {
-		
-		return "SELECT FlightTbl.SerialNum, FlightTbl.AirPlaneTailNum, AirPortTbl.Country, AirPortTbl.City, AirPortTbl_1.Country, AirPortTbl_1.City, FlightTbl.DepatureTime, FlightTbl.LandingTime, FlightTbl.Status, AirPortTbl.airportCode, AirPortTbl_1.airportCode\r\n"
-				+ "FROM AirPortTbl INNER JOIN (AirPortTbl AS AirPortTbl_1 INNER JOIN FlightTbl ON AirPortTbl_1.airportCode = FlightTbl.DestinationAirportID) ON AirPortTbl.airportCode = FlightTbl.DepatureAirportID\r\n"
-				+ "WHERE (((FlightTbl.DepatureTime)>= " + date1 + "  And (FlightTbl.DepatureTime)<= " + date2 + ") AND ((FlightTbl.LandingTime)>= " + date1 + " And (FlightTbl.LandingTime)<= " + date2 + "))\r\n"
-				+ "GROUP BY FlightTbl.SerialNum, FlightTbl.AirPlaneTailNum, AirPortTbl.Country, AirPortTbl.City, AirPortTbl_1.Country, AirPortTbl_1.City, FlightTbl.DepatureTime, FlightTbl.LandingTime, FlightTbl.Status, AirPortTbl.airportCode, AirPortTbl_1.airportCode\r\n"
-				+ "ORDER BY AirPortTbl_1.Country DESC , AirPortTbl_1.City DESC , FlightTbl.DepatureTime DESC , FlightTbl.LandingTime DESC;";
-	}
+	/*----------------------------------------- FLIGHTS QUERIES -----------------------------------------*/
+	public static final String SQL_SEL_FLIGHT = "SELECT * FROM FlightTbl";
+	public static final String SQL_DEL_FLIGHT = "{ call qryDelFlight(?) }";
+	public static final String SQL_INS_FLIGHT = "{ call qryInsFlight(?,?,?,?,?,?,?,?,?,?) }";
+	public static final String SQL_UPD_FLIGHT = "{ call qryUpdFlight(?,?,?,?,?,?,?,?,?,?,?) }";
+
+	/*----------------------------------------- AIRPORTS QUERIES -----------------------------------------*/
+	public static final String SQL_SEL_AIRPORT = "SELECT * FROM AirPortTbl";
 	
-	/**
-	 * String that present a query to return flights in range of to dates and that has ta list input amount of tourists seats
-	 * @param date1 = from date String
-	 * @param date2 = until date String
-	 * @param num = tourists seat
-	 * @return String that present a query
-	 */
-	public static  String qryBiggestFlightsStr(String date1, String date2, int num) {
-		
-		return "SELECT FlightTbl.SerialNum, AirPortTbl.Country, AirPortTbl.City, AirPortTbl_1.Country, AirPortTbl_1.City, FlightTbl.DepatureTime, FlightTbl.LandingTime, FlightTbl.Status\r\n"
-				+ "FROM AirPortTbl INNER JOIN (qryCntTouristsInPlane INNER JOIN (AirPortTbl AS AirPortTbl_1 INNER JOIN FlightTbl ON AirPortTbl_1.airportCode = FlightTbl.DestinationAirportID) ON qryCntTouristsInPlane.TailNum = FlightTbl.AirPlaneTailNum) ON AirPortTbl.airportCode = FlightTbl.DepatureAirportID\r\n"
-				+ "WHERE (((FlightTbl.DepatureTime)>= " + date1 + " And (FlightTbl.DepatureTime)<= " + date2 + " ) AND ((FlightTbl.LandingTime)>= " + date1 + " And (FlightTbl.LandingTime)<= " + date2 + " ) AND ((qryCntTouristsInPlane.CountOfID)>= " + num + " ))\r\n"
-				+ "GROUP BY FlightTbl.SerialNum, AirPortTbl.Country, AirPortTbl.City, AirPortTbl_1.Country, AirPortTbl_1.City, FlightTbl.DepatureTime, FlightTbl.LandingTime, FlightTbl.Status\r\n"
-				+ "ORDER BY AirPortTbl_1.Country DESC , AirPortTbl_1.City DESC , FlightTbl.DepatureTime DESC , FlightTbl.LandingTime DESC;";
-	} 
-	/*-----------------------------------------------------------------------------------------------------*/		
-	
+	/*----------------------------------------- AIRPLANES QUERIES -----------------------------------------*/
+	public static final String SQL_SEL_AIRPLANE = "SELECT * FROM AirPlaneTbl";
 	
 	
 	
@@ -123,18 +93,5 @@ public class Consts {
 			return null;
 		}
 	}
-	
-	/*----------------------------------------- FLIGHTS QUERIES -----------------------------------------*/
-	public static final String SQL_SEL_FLIGHT = "SELECT * FROM FlightTbl";
-	public static final String SQL_DEL_FLIGHT = "{ call qryDelFlight(?) }";
-	public static final String SQL_INS_FLIGHT = "{ call qryInsFlight(?,?,?,?,?,?,?,?,?,?) }";
-	public static final String SQL_UPD_FLIGHT = "{ call qryUpdFlight(?,?,?,?,?,?,?,?,?,?,?) }";
-
-	/*----------------------------------------- AIRPORTS QUERIES -----------------------------------------*/
-	public static final String SQL_SEL_AIRPORT = "SELECT * FROM AirPortTbl";
-	
-	/*----------------------------------------- AIRPLANES QUERIES -----------------------------------------*/
-	public static final String SQL_SEL_AIRPLANE = "SELECT * FROM AirPlaneTbl";
-	
 }
 
