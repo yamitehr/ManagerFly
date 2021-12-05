e97b2218661cffacfaaa4f1ff4c54da48e6a27f4
AvivLugasi <avivlugasi541@gmail.com>
2021-12-05 18:01:39 +0200

    addedd a runnable jar file and a batch file

--- ManagerFly/src/boundery/MainMenuFrm.java
+++ ManagerFly/src/boundery/MainMenuFrm.java
@@ -23,35 +23,35 @@ public class MainMenuFrm {
     @FXML
     void goAirPorts(ActionEvent event) throws IOException {
     	
-    	Pane root = LoadPane.LoadFXML(getClass(), "../boundery/AirPortsFrm.fxml");
+    	Pane root = LoadPane.LoadFXML(getClass(), "/boundery/AirPortsFrm.fxml");
     	pannelRoot.setCenter(root);
     }
 
     @FXML
     void goFlights(ActionEvent event) throws IOException {
     	
-    	Pane root = LoadPane.LoadFXML(getClass(), "../boundery/FlightsFrm.fxml");
+    	Pane root = LoadPane.LoadFXML(getClass(), "/boundery/FlightsFrm.fxml");
     	pannelRoot.setCenter(root);
     }
 
     @FXML
     void goHome(ActionEvent event) throws IOException {
     	
-    	Pane root = LoadPane.LoadFXML(getClass(), "../boundery/HomePage.fxml");
+    	Pane root = LoadPane.LoadFXML(getClass(), "/boundery/HomePage.fxml");
     	pannelRoot.setCenter(root);
     }
 
     @FXML
     void goPlanes(ActionEvent event) throws IOException {
     	
-    	Pane root = LoadPane.LoadFXML(getClass(), "../boundery/AirPlanesFrm.fxml");
+    	Pane root = LoadPane.LoadFXML(getClass(), "/boundery/AirPlanesFrm.fxml");
     	pannelRoot.setCenter(root);
     }
 
     @FXML
     void goReports(ActionEvent event) throws IOException {
     	
-    	Pane root = LoadPane.LoadFXML(getClass(), "../boundery/ReprotsFrm.fxml");
+    	Pane root = LoadPane.LoadFXML(getClass(), "/boundery/ReprotsFrm.fxml");
     	pannelRoot.setCenter(root);
     }
 
