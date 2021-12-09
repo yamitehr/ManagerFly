f16bdc06e0e757eaada57a9d06289d7ccf15a658
yamitehr <yamitehrlich@gmail.com>
2021-12-10 00:01:01 +0200

    no message

--- ManagerFly/ManageSystemWithVMArgs.bat
+++ ManagerFly/ManageSystemWithVMArgs.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-java --module-path "javafx-sdk-16\lib" --add-modules javafx.controls,javafx.fxml -jar ManageSystem.jar
+java --module-path "javafx-sdk-16\lib" --add-modules javafx.controls,javafx.fxml -jar ManagerFly.jar
 PAUSE
--- ManagerFly/ManagerSysWithoutVMArgs.bat
+++ ManagerFly/ManagerSysWithoutVMArgs.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-java  -jar ManageSystem.jar
+java  -jar ManagerFly.jar
 PAUSE
